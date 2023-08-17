4bf262b9c338b082653d325e44855018a4ebbda6
p0hoon <140869222+p0hoon@users.noreply.github.com>
2023-08-17 19:39:31 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Halo Liaaa </br> 私は本当に、本当にあなたを愛して",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+    "Maaf ya udah ngecewain kamu, maaf atas perbuatan aku yang kayak gini, apakah kah kamu memaafkan aku?",
+  buttonYes: "Iyaaa dimaafin",
+  buttonNo: "Ga Mw",
+  titleModar: "Terimakasih Lia",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Halo hehe maaf ya selama ini aku udah banyak ngecewain kamu, bikin kamu ngambek, bikin kamu badmood, bikin kamu marah, dll, aku sangat amat menyesal, sekarang aku sedang berusaha merubah diriku biar ga ngecewain kamu hehe, makasih love you<3",
 };
